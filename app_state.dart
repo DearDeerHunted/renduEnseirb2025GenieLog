b2424609268ae0595f47c9a6cb60f7ee0517e896
DearDeerHunted <isenhunt@gmail.com>
2025-12-18 12:06:16 +0100

    move server/model init & getSystemPrompt (AppState->Client)

--- app_state.dart
+++ app_state.dart
@@ -11,22 +11,12 @@ Future<void> loadClient(String clientName) async {
     List<String> keys = _getFromEnv("API_KEY");
     List<String> models = _getFromEnv("MODEL");
 
-    _server = await createMcpServer();
-    String systemPrompt = await getSystemPrompt(clientName);
     final firstModel = models.removeAt(0);
     final firstKey = keys.removeAt(0);
-    
-    final gga.GenerativeModel model = gga.GenerativeModel(
-      model: firstModel,
-      apiKey: firstKey,
-      systemInstruction: gga.Content.system(systemPrompt),
-    );
 
     _client = Client(
-      model: model,
-      systemPromptBuilder: () async => await getSystemPrompt(clientName),
+      clientName: clientName,
       firstModel: firstModel,
-      server: _server,
       fallbackModels: models,
       fallbackApiKeys: keys,
       apiKey: firstKey
@@ -53,23 +43,4 @@ List<String> _getFromEnv(String prefix) {
     throw Exception("$prefix manquante dans .env");
   }
   return results;
-}
-
-Future<String> getSystemPrompt(String clientName) async {
-  log.info('=== getSystemPrompt ==='.padRight(20));
-  switch (clientName) {
-    case "general":
-      return getGeneralSystemPrompt();
-    case "smart":
-      return getSmartSystemPrompt();
-    case "Exemple":
-      try {
-        String info = await _server.callTool('get_current_location', {});
-        return getExempleSystemPrompt(info);
-      } catch (e) {
-        return getExempleSystemPrompt("Error: $e");
-      }
-    default:
-      throw Exception("Unknown client type: $clientName");
-  }
 }
--- client.dart
+++ client.dart
@@ -1,30 +1,58 @@
 class Client {
-  gga.GenerativeModel _model;
-  Future<String> Function() systemPromptBuilder;
-  final LocalMcpServer server;
+  final Future<LocalMcpServer> _server = createMcpServer();
+
   final List<String> fallbackModels;
   final List<String> fallbackApiKeys;
+
+  gga.GenerativeModel? _model;
   String _currentModelName;
   String _currentApiKey;
+  final String _clientName;
 
   final List<gga.Content> convHistory = [];
   static final int historySize = 500;
 
   Client({
-    required gga.GenerativeModel model,
-    required this.systemPromptBuilder,
+    required String clientName,
     required String firstModel,
-    required this.server,
     required this.fallbackModels,
     required this.fallbackApiKeys,
     required String apiKey,
-  }) : _model = model, _currentModelName = firstModel, _currentApiKey = apiKey;
+  }) :
+  _clientName = clientName,
+  _currentModelName = firstModel,
+  _currentApiKey = apiKey
+  {
+    _refreshModel();
+  }
 
   void _refreshModel() async {
     _model = gga.GenerativeModel(
       model: _currentModelName,
       apiKey: _currentApiKey,
-      systemInstruction: gga.Content.system(await systemPromptBuilder()), 
+      systemInstruction: gga.Content.system(await getSystemPrompt()), 
     );
   }
+
+  Future<String> getSystemPrompt() async {
+  log.info('=== getSystemPrompt ==='.padRight(20));
+
+  final serverInstance = await _server;
+  
+  switch (_clientName) {
+    case "general":
+      return getGeneralSystemPrompt();
+    case "smart":
+      return getSmartSystemPrompt();
+    case "Exemple":
+      try {
+        String info = await serverInstance.callTool('get_current_location', {});
+        return getExempleSystemPrompt(info);
+      } catch (e) {
+        return getExempleSystemPrompt("Error: $e");
+      }
+    default:
+      throw Exception("Unknown client type: $_clientName");
+  }
+}
 }
