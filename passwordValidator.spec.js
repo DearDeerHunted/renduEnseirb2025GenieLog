312f35aa0628088706ef053da2e57a0dab3e2245
DearDeerHunted <isenhunt@gmail.com>
2025-12-19 11:45:00 +0100

    add test min nb letter in pwd

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,11 +1,16 @@
 const checkPWD = require("./passwordValidator");
 
 test('le mot de passe doit contenir au moins 8 caractères', () => {
-  expect(checkPWD('1234567')).toBe(false);
-  expect(checkPWD('12345678')).toBe(true);
+  expect(checkPWD('123456A')).toBe(false);
+  expect(checkPWD('1234567A')).toBe(true);
 });
 
 test('le mot de passe contient au moins 1 chiffre', () => {
   expect(checkPWD('password')).toBe(false);
   expect(checkPWD('password1')).toBe(true);
+});
+
+test('le mot de passe contient au moins 1 lettre', () => {
+  expect(checkPWD('12345678')).toBe(false);
+  expect(checkPWD('1234567A')).toBe(true);
 });
