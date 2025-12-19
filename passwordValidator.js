bc69775eac03044c8f6d93b0cbb2597f9ddba43d
DearDeerHunted <isenhunt@gmail.com>
2025-12-19 15:01:06 +0100

    pwd valiadtor can be param

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,9 +1,19 @@
-function checkPWD(pwd) {
-    return (
-        pwd.length >= 8 &&
-        /\d/.test(pwd) &&
-        /[a-zA-Z]/.test(pwd)
-    );
+function checkPWD(pwd, options) {
+    const minLen = options.minLen;
+    const minLet = options.minLet;
+    const minNum = options.minNum;
+    const newRules = options.newRules || [];
+
+    const letCount = (pwd.match(/[a-zA-Z]/g) || []).length
+    const numCount = (pwd.match(/\d/g) || []).length
+
+    const lenCheck = (pwd.length >= minLen);
+    const letCheck = (letCount >= minLet);
+    const numCheck = (numCount >= minNum);
+
+    const newCheck = newRules.every(rule => rule(pwd));
+
+    return lenCheck && letCheck && numCheck && newCheck
 }
 
 module.exports = checkPWD;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,16 +1,31 @@
 const checkPWD = require("./passwordValidator");
 
 test('le mot de passe doit contenir au moins 8 caractères', () => {
-  expect(checkPWD('123456A')).toBe(false);
-  expect(checkPWD('1234567A')).toBe(true);
+  expect(checkPWD('123456A', {minLen: 8, minLet: 1, minNum: 1})).toBe(false);
+  expect(checkPWD('1234567A', {minLen: 8, minLet: 1, minNum: 1})).toBe(true);
 });
 
 test('le mot de passe contient au moins 1 chiffre', () => {
-  expect(checkPWD('password')).toBe(false);
-  expect(checkPWD('password1')).toBe(true);
+  expect(checkPWD('password', {minLen: 8, minLet: 1, minNum: 1})).toBe(false);
+  expect(checkPWD('password1', {minLen: 8, minLet: 1, minNum: 1})).toBe(true);
 });
 
 test('le mot de passe contient au moins 1 lettre', () => {
-  expect(checkPWD('12345678')).toBe(false);
-  expect(checkPWD('1234567A')).toBe(true);
+  expect(checkPWD('12345678', {minLen: 8, minLet: 1, minNum: 1})).toBe(false);
+  expect(checkPWD('1234567A', {minLen: 8, minLet: 1, minNum: 1})).toBe(true);
+});
+
+test('le mot de passe avec longueur paramétrable', () => {
+    expect(checkPWD('1234567A', {minLen: 10, minLet: 1, minNum: 1})).toBe(false);
+    expect(checkPWD('1234A', {minLen: 5, minLet: 1, minNum: 1})).toBe(true);
+});
+
+test('le mot de passe avec nombre minimal de chiffre paramétrable', () => {
+  expect(checkPWD('1234567A', {minLen: 8, minLet: 2, minNum: 1})).toBe(false);
+  expect(checkPWD('12345678', {minLen: 8, minLet: 0, minNum: 1})).toBe(true);
+});
+
+test('le mot de passe avec nombre minimal de lettre paramétrable', () => {
+  expect(checkPWD('password1', {minLen: 8, minLet: 1, minNum: 2})).toBe(false);
+  expect(checkPWD('password', {minLen: 8, minLet: 1, minNum: 0})).toBe(true);
 });
