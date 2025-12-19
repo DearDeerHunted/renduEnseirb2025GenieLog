2b22074d87aee4780233e94fa64fc675942725f5
DearDeerHunted <isenhunt@gmail.com>
2025-12-19 11:45:44 +0100

    check all test for min nb letter in pwd

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,5 +1,9 @@
 function checkPWD(pwd) {
-    return pwd.length >= 8 && /\d/.test(pwd);
+    return (
+        pwd.length >= 8 &&
+        /\d/.test(pwd) &&
+        /[a-zA-Z]/.test(pwd)
+    );
 }
 
 module.exports = checkPWD;
