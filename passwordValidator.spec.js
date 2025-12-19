79895210cf74d86623d2e3f2ce4279deb2966145
DearDeerHunted <isenhunt@gmail.com>
2025-12-19 11:35:45 +0100

    add test min nb number in pwd

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -4,3 +4,8 @@ test('le mot de passe doit contenir au moins 8 caractères', () => {
   expect(checkPWD('1234567')).toBe(false);
   expect(checkPWD('12345678')).toBe(true);
 });
+
+test('le mot de passe contient au moins 1 chiffre', () => {
+  expect(checkPWD('password')).toBe(false);
+  expect(checkPWD('password1')).toBe(true);
+});
