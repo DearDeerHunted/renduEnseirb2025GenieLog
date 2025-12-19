3c93f8aa9452f4cd8cc1e8762fae08fc3a9159e8
DearDeerHunted <isenhunt@gmail.com>
2025-12-19 18:00:31 +0100

    add debitAccount

--- bankDAO.js
+++ bankDAO.js
@@ -1,6 +1,11 @@
 const bankDAO = {
   retrieveBalance(accountId) {
     console.log(`Retrieve balance for account ${accountId}`);
+    return 1000;
+  },
+
+  debitAccount(accountId, amount) {
+    console.log(`Debiting ${amount} from account ${accountId}`);
   }
 };
 
