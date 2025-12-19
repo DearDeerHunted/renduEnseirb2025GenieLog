9a22c9d71e9b43c8af378cca4dc18b74d10ec5af
DearDeerHunted <isenhunt@gmail.com>
2025-12-19 11:28:58 +0100

    add test to min carac nb in pwd

--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -2,4 +2,5 @@ const checkPWD = require("./passwordValidator");
 
 test('le mot de passe doit contenir au moins 8 caractères', () => {
   expect(checkPWD('1234567')).toBe(false);
+  expect(checkPWD('12345678')).toBe(true);
 });
