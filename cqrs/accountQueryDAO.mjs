36f9b0b770fe88f931dbfcaffdc391c67fd45fc4
DearDeerHunted <isenhunt@gmail.com>
2026-01-06 16:47:36 +0100

    div DAO into command or query

--- cqrs/accountQueryDAO.mjs
+++ cqrs/accountQueryDAO.mjs
@@ -1,1 +1,21 @@
-export const accountQueryDAO = {};
+import { ACCOUNT_LIST } from "./database.mjs";
+
+export const accountQueryDAO = {
+    retrieveAccountList() {
+        return ACCOUNT_LIST.map(({ creationDate, ...accountWithoutDate }) => accountWithoutDate);
+    },
+    retrieveAccount(id) {
+        const account = ACCOUNT_LIST.find(a => a.id === id);
+        if (account) {
+          return {
+            id: account.id,
+            name: `${account.lastName} ${account.firstName}`,
+            creationDate: account.creationDate
+          };
+        }
+        else {
+          console.log("retrieveAccount: no account with this id")
+          return null;
+        }
+    },
+};
