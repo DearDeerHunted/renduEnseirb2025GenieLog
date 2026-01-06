9bfe2a1350afc78070380653b14e859cbd1df486
DearDeerHunted <isenhunt@gmail.com>
2026-01-06 14:53:00 +0100

    impl insertAccount in DAO

--- crud/accountDAO.mjs
+++ crud/accountDAO.mjs
@@ -1,7 +1,9 @@
 import { ACCOUNT_LIST } from "./database.mjs";
 
 export const accountDAO = {
-  insertAccount(account) {},
+  insertAccount(account) {
+    ACCOUNT_LIST.push(account);
+  },
   retrieveAccountList() {},
   updateAccount(account) {},
   retrieveAccount(id) {},
