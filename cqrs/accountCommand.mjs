689fb84c1cd0e5ac9147f66a571b2c7d2500692e
DearDeerHunted <isenhunt@gmail.com>
2026-01-06 16:47:03 +0100

    div services into command or query

--- cqrs/accountCommand.mjs
+++ cqrs/accountCommand.mjs
@@ -1,1 +1,23 @@
-export const accountCommand = {};
+import { Account } from "./account.mjs";
+import { accountCommandDAO } from "./accountCommandDAO.mjs";
+
+export const accountCommand = {
+    addAccount(lastName, firstName) {
+        const newAccount = new Account(undefined, lastName, firstName);
+        accountCommandDAO.insertAccount(newAccount);
+        return newAccount;
+    },
+    saveAccount(id, lastName, firstName) {
+        const account = accountCommandDAO.restoreAccount(id);
+        if (account) {
+            account.lastName = lastName;
+            account.firstName = firstName;
+            accountCommandDAO.updateAccount(account);
+            return account;
+        }
+        else {
+            console.log("saveAccount: no account with this id")
+            return null;
+        }
+    },
+};
--- cqrs/accountQuery.mjs
+++ cqrs/accountQuery.mjs
@@ -1,1 +1,10 @@
-export const accountQuery = {};
+import { accountQueryDAO } from "./accountQueryDAO.mjs";
+
+export const accountQuery = {
+    getAccountList() {
+        return accountQueryDAO.retrieveAccountList();
+    },
+    getAccount(id) {
+        return accountQueryDAO.retrieveAccount(id);
+    },
+};
