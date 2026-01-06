0e47b9d01e2ef34caffd2b9b0cca9a97644848c9
DearDeerHunted <isenhunt@gmail.com>
2026-01-06 14:57:56 +0100

    call getAccountList in client

--- crud/client.mjs
+++ crud/client.mjs
@@ -3,3 +3,7 @@ import { accountService } from "./accountService.mjs";
 console.log("addAccount")
 const newAccount = accountService.addAccount('G','A')
 console.log(newAccount)
+
+console.log("getAccountList")
+for (const account of accountService.getAccountList())
+    console.log(account)
