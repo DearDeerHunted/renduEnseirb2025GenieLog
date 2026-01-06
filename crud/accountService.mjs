95d1b95a897aca23cfab3b52be16f8bbe299ec03
DearDeerHunted <isenhunt@gmail.com>
2026-01-06 15:22:56 +0100

    corr args in addAccount & impl saveAccount

--- crud/accountService.mjs
+++ crud/accountService.mjs
@@ -3,13 +3,26 @@ import { accountDAO } from "./accountDAO.mjs";
 
 export const accountService = {
   addAccount(lastName, firstName) {
-    const newAccount = new Account(lastName=lastName, firstName=firstName);
+    const newAccount = new Account(undefined, lastName, firstName);
     accountDAO.insertAccount(newAccount);
     return newAccount;
   },
   getAccountList() {
     return accountDAO.retrieveAccountList();
   },
-  saveAccount(id, lastName, firstName) {},
+  saveAccount(id, lastName, firstName) {
+    const accountList = this.getAccountList();
+    const account = accountList.find((a) => a.id === id);
+    if (account) {
+      account.lastName = lastName;
+      account.firstName = firstName;
+      accountDAO.updateAccount(account);
+      return account;
+    }
+    else {
+      console.log("saveAccount: no account with this id")
+      return null;
+    }
+  },
   getAccount(id) {},
 };
