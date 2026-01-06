f36eaf5c9dbc5ea4b318eff0ad9439f98b575596
DearDeerHunted <isenhunt@gmail.com>
2026-01-06 14:57:35 +0100

    impl getAccountList in service

--- crud/accountService.mjs
+++ crud/accountService.mjs
@@ -7,7 +7,9 @@ export const accountService = {
     accountDAO.insertAccount(newAccount);
     return newAccount;
   },
-  getAccountList() {},
+  getAccountList() {
+    return accountDAO.retrieveAccountList();
+  },
   saveAccount(id, lastName, firstName) {},
   getAccount(id) {},
 };
