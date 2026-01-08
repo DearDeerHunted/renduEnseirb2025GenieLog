e99ba2dabe03963b9d772f260e3d5726f7d1c4b1
DearDeerHunted <isenhunt@gmail.com>
2026-01-08 10:50:43 +0100

    account reconstruction

--- eventSourcing/accountCommandDAO.mjs
+++ eventSourcing/accountCommandDAO.mjs
@@ -3,10 +3,21 @@ import { eventList } from "./eventStore.mjs";
 
 export const accountCommandDAO = {
     restoreAccount(id){
-        const creationEvent = eventList.find(e => e.name === 'accountAdded' && e.accountId === id);
-        if (!creationEvent) return null;
+        const accountEvents = eventList.filter(e => e.accountId === id);
+        if (accountEvents.length === 0) return null;
 
-        const account = creationEvent.payload;
-        return new Account(account.id, account.lastName, account.firstName, account.creationDate);
+        let account = null;
+
+        for (const event of accountEvents) {
+          if (event.name === 'accountAdded') {
+            const data = event.payload;
+            account = new Account(data.id, data.lastName, data.firstName, data.creationDate);
+          } 
+          else if (event.name === 'accountUpdated' && account) {
+            account.lastName = event.payload.lastName;
+            account.firstName = event.payload.firstName;
+          }
+        }
+        return account;
     },
 };
