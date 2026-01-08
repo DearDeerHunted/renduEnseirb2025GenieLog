142ae058a2c713c0b1c602129b63aa267ed4bcd3
DearDeerHunted <isenhunt@gmail.com>
2026-01-08 11:13:22 +0100

    modif restoreAccount using reduce

--- eventSourcing/accountCommandDAO.mjs
+++ eventSourcing/accountCommandDAO.mjs
@@ -3,21 +3,19 @@ import { eventList } from "./eventStore.mjs";
 
 export const accountCommandDAO = {
     restoreAccount(id){
-        const accountEvents = eventList.filter(e => e.accountId === id);
-        if (accountEvents.length === 0) return null;
-
-        let account = null;
-
-        for (const event of accountEvents) {
-          if (event.name === 'accountAdded') {
-            const data = event.payload;
-            account = new Account(data.id, data.lastName, data.firstName, data.creationDate);
-          } 
-          else if (event.name === 'accountUpdated' && account) {
-            account.lastName = event.payload.lastName;
-            account.firstName = event.payload.firstName;
-          }
-        }
-        return account;
+        return eventList
+          .filter(e => e.accountId === id)
+          .reduce((account, event) => {
+            if (event.name === 'accountAdded') {
+              const data = event.payload;
+              return new Account(data.id, data.lastName, data.firstName, data.creationDate);
+            } 
+            if (event.name === 'accountUpdated') {
+              account.lastName = event.payload.lastName;
+              account.firstName = event.payload.firstName;
+              return account;
+            }
+            return account;
+          }, null);
     },
 };
