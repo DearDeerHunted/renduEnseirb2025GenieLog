426a4cf97d904a0ed8ee057b9dff92728147803c
DearDeerHunted <isenhunt@gmail.com>
2026-01-08 08:54:01 +0100

    maj retrieveAccount: retrieve account from cache

--- cqrs/accountQueryDAO.mjs
+++ cqrs/accountQueryDAO.mjs
@@ -1,18 +1,15 @@
 import { ACCOUNT_LIST } from "./database.mjs";
 import { accountSummaryList } from "./queryDatabase.mjs";
+import { accountCache } from "./cache.mjs";
 
 export const accountQueryDAO = {
     retrieveAccountList() {
       return accountSummaryList;
     },
     retrieveAccount(id) {
-        const account = ACCOUNT_LIST.find(a => a.id === id);
+        const account = accountCache[id];
         if (account) {
-          return {
-            id: account.id,
-            name: `${account.lastName} ${account.firstName}`,
-            creationDate: account.creationDate
-          };
+          return account;
         }
         else {
           console.log("retrieveAccount: no account with this id")
